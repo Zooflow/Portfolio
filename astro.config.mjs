47e6042c8a9920b0deffcb41024c52878f5ff56f
Zooflow <zooflowcr@gmail.com>
2025-04-01 23:08:47 -0300

    V2

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
+import lottie from "astro-integration-lottie";
 
 // https://astro.build/config
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()],
       },
+    integrations: [
+      lottie(), // <-- add integration
+    ]
 });
